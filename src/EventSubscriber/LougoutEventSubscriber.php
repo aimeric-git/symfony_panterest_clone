84351df030697c7ae4564c5e62987cd8756f297f
aimeric <aimericjjb@gmail.com>
2021-04-08 10:31:43 +0300

    display flash message on logout

---
+++ src/EventSubscriber/LougoutEventSubscriber.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGenerator;
+use Symfony\Component\Security\Http\Event\LogoutEvent; 
+
+class LougoutEventSubscriber implements EventSubscriberInterface
+{
+    private $urlGenerator;
+
+    public function __constructor(UrlGenerator $urlGenerator)
+    {
+        $this->urlGenerator = $urlGenerator; 
+    }
+
+    public function onLogoutEvent(LogoutEvent $event)
+    {
+        $event->getRequest()->getSession()->getFlashBag()->add(
+            'success', 
+            'Logged out successfully'
+        );
+        
+        // $event->setResponse(new RedirectResponse($this->urlGenerator->generate('app_index')));
+
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            LogoutEvent::class => 'onLogoutEvent',
+        ];
+    }
+}
